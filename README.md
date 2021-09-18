7fa5a86cad0d623534bc7c8bae8dd9ea98e286c1
Kishan Takoordyal <kishan@kinesis.games>
2021-09-18 18:02:46 +0400

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Little experiment with Rust 🦀 WASM + JS
+
+![](https://backend.kinesis.games/a403b240-1887-11ec-80ce-b9191fb40fb5-Opera%20Snapshot_2021-09-18_175137_localhost.png)
+
+### Running this demo locally
+
+```bash
+git clone https://github.com/EdgeKing810/wasm-experiment.git
+
+cd wasm-experiment/
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+wasm-pack build
+
+cd www/
+npm i
+npm start
+```
+
+### Resources
+
+- https://rustwasm.github.io/docs/book/introduction.html
---
+++ www/index.js
@@ -0,0 +1,79 @@
+import { Universe, Cell } from 'wasm-game-of-life';
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
+
+const universe = Universe.new(50, 50);
+const width = universe.get_width();
+const height = universe.get_height();
+
+const CELL_SIZE = 15;
+const GRID_COLOR = '#CCCCCC';
+const DEAD_COLOR = '#FFFFFF';
+const ALIVE_COLOR = '#000000';
+
+const canvas = document.getElementById('game-of-life-canvas');
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  for (let y = 0; y <= width; y++) {
+    ctx.moveTo(y * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(y * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  for (let x = 0; x <= height; x++) {
+    ctx.moveTo(0, x * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, x * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const bitIsSet = (n, arr) => {
+  const byte = Math.floor(n / 8);
+  const mask = 1 << n % 8;
+  return (arr[byte] & mask) === mask;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.get_cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, (width * height) / 8);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = bitIsSet(idx, cells) ? ALIVE_COLOR : DEAD_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+const renderLoop = () => {
+  universe.tick();
+
+  drawGrid();
+  drawCells();
+};
+
+drawGrid();
+drawCells();
+setInterval(() => renderLoop(), 30);
